01318780ed030f103024ba021ca071711ab8c771
DSOTec <sdamiloladaniel@gmail.com>
2025-09-05 05:16:31 +0100

    edited the hardhatConfig file

---
+++ Contracts/hardhat.config.ts
@@ -0,0 +1,36 @@
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+import { vars } from "hardhat/config";
+
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
+const ARBISCAN_API_KEY = vars.get("ARBISCAN_API_KEY", "");
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.20",
+  networks: {
+    arbitrumSepolia: {
+      url: "https://sepolia-rollup.arbitrum.io/rpc",
+      accounts: [PRIVATE_KEY],
+      chainId: 421614,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      arbitrumSepolia: ARBISCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "arbitrumSepolia",
+        chainId: 421614,
+        urls: {
+          apiURL: "https://api-sepolia.arbiscan.io/api",
+          browserURL: "https://sepolia.arbiscan.io",
+        },
+      },
+    ],
+  },
+};
+
+export default config;
